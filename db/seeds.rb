fc0525fe4df5d0120b2584cbe4066c5218ffabc8
Donald Brooks Arum <brooksd079@gmail.com>
2022-09-24 23:47:58 +0300

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Student.create(first_name: "Donald", last_name: "Brooks", grade: 90)
